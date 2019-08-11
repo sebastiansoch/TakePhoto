930ea1a208b37b63cde9bd584d24438783467472
Sebastian Soch <sebastian.soch@gmail.com>
2019-08-11 18:49:12 +0200

    Write Photo Permission

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/customview/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/customview/R.java
@@ -86,26 +86,26 @@ public final class R {
         public static final int async = 0x7f08001d;
         public static final int blocking = 0x7f080020;
         public static final int chronometer = 0x7f08002a;
-        public static final int forever = 0x7f08003f;
-        public static final int icon = 0x7f080045;
-        public static final int icon_group = 0x7f080046;
-        public static final int info = 0x7f080049;
-        public static final int italic = 0x7f08004c;
-        public static final int line1 = 0x7f08004e;
-        public static final int line3 = 0x7f08004f;
-        public static final int normal = 0x7f080057;
-        public static final int notification_background = 0x7f080058;
-        public static final int notification_main_column = 0x7f080059;
-        public static final int notification_main_column_container = 0x7f08005a;
-        public static final int right_icon = 0x7f080064;
-        public static final int right_side = 0x7f080065;
-        public static final int tag_transition_group = 0x7f080086;
-        public static final int tag_unhandled_key_event_manager = 0x7f080087;
-        public static final int tag_unhandled_key_listeners = 0x7f080088;
-        public static final int text = 0x7f080089;
-        public static final int text2 = 0x7f08008a;
-        public static final int time = 0x7f08008e;
-        public static final int title = 0x7f080091;
+        public static final int forever = 0x7f080040;
+        public static final int icon = 0x7f080046;
+        public static final int icon_group = 0x7f080047;
+        public static final int info = 0x7f08004a;
+        public static final int italic = 0x7f08004d;
+        public static final int line1 = 0x7f08004f;
+        public static final int line3 = 0x7f080050;
+        public static final int normal = 0x7f080058;
+        public static final int notification_background = 0x7f080059;
+        public static final int notification_main_column = 0x7f08005a;
+        public static final int notification_main_column_container = 0x7f08005b;
+        public static final int right_icon = 0x7f080065;
+        public static final int right_side = 0x7f080066;
+        public static final int tag_transition_group = 0x7f080087;
+        public static final int tag_unhandled_key_event_manager = 0x7f080088;
+        public static final int tag_unhandled_key_listeners = 0x7f080089;
+        public static final int text = 0x7f08008a;
+        public static final int text2 = 0x7f08008b;
+        public static final int time = 0x7f08008f;
+        public static final int title = 0x7f080092;
     }
     public static final class integer {
         private integer() {}
@@ -125,7 +125,7 @@ public final class R {
     public static final class string {
         private string() {}
 
-        public static final int status_bar_notification_info_overflow = 0x7f0c002e;
+        public static final int status_bar_notification_info_overflow = 0x7f0c002f;
     }
     public static final class style {
         private style() {}
--- app/src/main/java/com/sebastiansoch/takephoto/MainActivity.java
+++ app/src/main/java/com/sebastiansoch/takephoto/MainActivity.java
@@ -1,8 +1,14 @@
 package com.sebastiansoch.takephoto;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -10,10 +16,16 @@ import android.widget.Toast;
 import com.sebastiansoch.takephoto.info.CameraSettings;
 import com.sebastiansoch.takephoto.info.Schedule;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int PHOTO_SCHEDULER_REQ_CODE = 1;
     private static final int CAMERA_PREVIEW_REQ_CODE = 2;
+    private static final int WRITE_EXTERNAL_STORAGE_REQ_CODE = 3;
 
     private Schedule schedule;
     private CameraSettings cameraSettings;
@@ -30,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void openCameraPreview(View view) {
         startActivityForResult(new Intent(this, CameraPreview.class), CAMERA_PREVIEW_REQ_CODE);
+    }
+
+    public void executeSchedular(View view) {
+        checkWriteStoragePermission();
     }
 
     @Override
@@ -51,4 +67,53 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQ_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    createPhotoFileName();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(this, "Permission successfully granted!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "App needs to save video to run", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void checkWriteStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    createPhotoFileName();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    Toast.makeText(this, "App needs to be able to save photo", Toast.LENGTH_SHORT).show();
+                }
+
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_REQ_CODE);
+            }
+        } else {
+            try {
+                createPhotoFileName();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void createPhotoFileName() throws IOException {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String prefix = "TakePhoto_" + timestamp + "_";
+
+        File file = File.createTempFile(prefix, ".jpg", Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+    }
+
 }
